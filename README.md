ffda332b5e886a53e008bc5471ba760305343f6e
AmanYadavFeb22 <ay415282@gmail.com>
2025-07-30 19:08:18 +0530

    globalThis vs global

---
+++ README.md
@@ -0,0 +1,4 @@
+# Node-JS-Learning-Series
+This is Node Js Learning Series-(A complete Backend learning Series)
+# Global vs globalThis
+If we do console.log(globalThis==global) // It gives us true
---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("helo world")
+console.log(this) // It gives us an Empty Object
+console.log(global) // It gives us an global Object
+console.log(globalThis)
+
+console.log(globalThis==global) // It gives us true
